f23018338b1aecae72fcfa4711ac2701acedd0ca
UIrich <rodorama3@gmail.com>
2024-10-09 13:54:06 -0300

    Update ProjetoOdonto.sql

--- Database/ProjetoOdonto.sql
+++ Database/ProjetoOdonto.sql
@@ -1,3 +1,9 @@
+USE master;
+GO
+
+DROP DATABASE IF EXISTS ProjetoOdonto;
+GO
+
 CREATE DATABASE ProjetoOdonto;
 GO
 
@@ -7,8 +13,8 @@ GO
 CREATE TABLE Convenio(
 	Conv_ID INT NOT NULL IDENTITY(1,1),
 	Conv_Nome VARCHAR(100) NOT NULL,
-	Conv_CNPJ VARCHAR(20) NOT NULL UNIQUE,
-	Conv_Telefone VARCHAR(15) NOT NULL,
+	Conv_CNPJ VARCHAR(14) NOT NULL UNIQUE,
+	Conv_Telefone VARCHAR(11) NOT NULL,
 	Conv_Endereco VARCHAR(255) NOT NULL,
 	Conv_Email VARCHAR(100) NOT NULL,
 	Conv_DataCriacao DATETIME NOT NULL DEFAULT GETDATE(),
@@ -20,11 +26,14 @@ GO
 CREATE TABLE Dentista(
 	Den_ID INT NOT NULL IDENTITY(1,1),
 	Den_Nome VARCHAR(100) NOT NULL,
-	Den_CRM VARCHAR(20) NOT NULL UNIQUE,
+	Den_CRM VARCHAR(6) NOT NULL UNIQUE,
+	Den_Estado CHAR(2) NOT NULL,
 	Den_Especialidade VARCHAR(100) NOT NULL,
-	Den_Telefone VARCHAR(15) NOT NULL,
+	Den_Telefone VARCHAR(11) NOT NULL,
 	Ativo_Inativo INT NOT NULL DEFAULT 1,
-	CONSTRAINT PK_Dentista PRIMARY KEY (Den_ID)
+	CONSTRAINT PK_Dentista PRIMARY KEY (Den_ID),
+	CONSTRAINT CK_Dentista_Estado CHECK (Den_Estado IN ('AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO'))
+
 );
 GO
 
@@ -44,9 +53,9 @@ CREATE TABLE Responsavel(
 	Res_Nome VARCHAR(100) NOT NULL,
 	Res_DataNascimento DATE NOT NULL,
 	Res_Idade INT NOT NULL,
-	Res_CPF VARCHAR(15) NOT NULL UNIQUE,
+	Res_CPF VARCHAR(11) NOT NULL UNIQUE,
 	Res_Sexo VARCHAR(30) NOT NULL,
-	Res_Telefone VARCHAR(15) NOT NULL,
+	Res_Telefone VARCHAR(11) NOT NULL,
 	Res_Parentesco VARCHAR(50) NOT NULL,
 	Ativo_Inativo INT NOT NULL DEFAULT 1,
 	CONSTRAINT PK_Responsavel PRIMARY KEY (Res_ID),
@@ -59,9 +68,9 @@ CREATE TABLE Paciente(
 	Pac_Nome VARCHAR(100) NOT NULL,
 	Pac_DataNascimento DATE NOT NULL,
 	Pac_Idade INT NOT NULL,
-	Pac_CPF VARCHAR(15) NOT NULL,
+	Pac_CPF VARCHAR(11) NOT NULL UNIQUE,
 	Pac_Sexo VARCHAR(30) NOT NULL,
-	Pac_Telefone VARCHAR(15) NOT NULL,
+	Pac_Telefone VARCHAR(11) NOT NULL,
 	Pac_Email VARCHAR(100),
 	Ativo_Inativo INT NOT NULL DEFAULT 1,
 	Pac_ConvenioID_FK INT,
@@ -109,12 +118,13 @@ CREATE TABLE Endereco(
 	End_Numero VARCHAR(10),
 	End_Cidade VARCHAR(70) NOT NULL,
 	End_Estado CHAR(2) NOT NULL,
-	End_CEP VARCHAR(10) NOT NULL,
+	End_CEP VARCHAR(8) NOT NULL,
 	End_Complemento VARCHAR(50),
 	End_PacienteID_FK INT NOT NULL,
 	CONSTRAINT PK_Endereco PRIMARY KEY (End_ID),
 	CONSTRAINT FK_Endereco_Paciente FOREIGN KEY (End_PacienteID_FK)
-	REFERENCES Paciente(Pac_ID)
+	REFERENCES Paciente(Pac_ID),
+	CONSTRAINT CK_Endereco_Estado CHECK (End_Estado IN ('AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO'))
 );
 GO
 
